9a772efba53cfcd72b3fe393c92302bd72b447f3
Patrice MAYET <patrice.mayet@gmail.com>
2020-04-15 18:25:43 +0200

    Add method to delete the given optionValue

--- src/Bigcommerce/Api/Client.php
+++ src/Bigcommerce/Api/Client.php
@@ -2053,4 +2053,16 @@ class Client
         $filter = Filter::create($filter);
         return self::getCollection('/products/'.$productId.'/skus' . $filter->toQuery(), 'Sku');
     }
+
+    /**
+     * Delete the given optionValue.
+     *
+     * @param int $optionId optionId
+     * @param int $id optionValueId
+     * @return mixed
+     */
+    public static function deleteOptionValue($optionId, $id)
+    {
+        return self::deleteResource('/options/' . $optionId .'/values/'. $id);
+    }
 }
